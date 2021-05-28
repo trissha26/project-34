361c68e091efdd50e0f96a12e3b558d70cbd5f52
trissha26 <73527457+trissha26@users.noreply.github.com>
2021-05-28 10:33:33 +0530

    Add files via upload
    monster game

--- monster.js
+++ monster.js
@@ -1,29 +1,28 @@
-class Hero {
+class Monster {
     constructor(x,y,r)
       {
           var options = { 
-              density: 1, 
-              frictionAir: 1
-          };
+        density: 5, 
+        frictionAir: 0
+      };
           this.x=x;
           this.y=y;
           this.r=r;
-          this.image=loadImage("superhero1.png");
-          this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+          this.image=loadImage("monster1.png");
+          this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
           World.add(world, this.body);
   
       }
-      
       display()
       {
               
-              var heroPos=this.body.position;		
+              var santaPos=this.body.position;		
               push()
-              translate(heroPos.x, heroPos.y-100);
+              translate(santaPos.x, santaPos.y-100);
               rectMode(CENTER)
               fill(255,0,255)
               imageMode(CENTER);
-              image(this.image, 0,0,this.r+150, this.r)
+              image(this.image, 0,0,this.r, this.r)
               pop()
               
       }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,62 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var hero, heropImg;
-var monster, monsterImg;
+var engine, world;
+var box1, box2, box3,box4;
+var hero,monster,rope,ground;
 
 function preload() {
-//preload the images here
-
-
+  bg = loadImage("gamingbackground2.png");
 }
 
 function setup() {
-  createCanvas(3000, 800);
-  // create sprites here
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
+
+  box1 = new Box(600, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(600, 100, 70, 70);
+  box6 = new Box(600, 100, 70, 70);
+  box7 = new Box(900, 100, 70, 70);
+  box8 = new Box(600, 100, 70, 70);
+  box9 = new Box(900, 100, 70, 70);
+  box10 = new Box(600, 100, 70, 70);
 
 }
 
 function draw() {
-  background(0);
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
 
+  hero.display();
+  rope.display();
+  monster.display();
+
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(hero.body, {x: mouseX, y: mouseY });
 }
 
